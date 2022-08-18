4ee16d4110856af87b9c2d577adc7c364ab74f47
Christian Bormann <chris.bormann@gmx.de>
2022-08-18 18:13:33 +0200

    wip: test program to query delta information (verify the problem on ledger)

---
+++ python-tests/test.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3.5
+import indy_vdr
+import requests
+import asyncio
+
+class Tester:
+    pool: indy_vdr.Pool
+    genesis: str
+
+    def __init__(self, genesis_url: str):
+        try: 
+            self.genesis = requests.get(genesis_url, allow_redirects=True).text
+            asyncio.get_event_loop().run_until_complete(self.open())
+        except Exception as err: 
+            print(f"Could not resolve genesis file: {err=}")
+            return None
+        
+    async def open(self):
+        try: 
+            pool = await indy_vdr.pool.open_pool(transactions=self.genesis)
+            self.pool = pool
+        except Exception as err:
+            print(f"Could not initialize pool: {err=}")
+    
+    async def get_from_tx(self, tx_id: int) -> dict:
+        tx_req = indy_vdr.ledger.build_get_txn_request(None, 1, tx_id)
+        tx_raw = await self.pool.submit_request(tx_req)
+        rev_reg_id = tx_raw["data"]["txn"]["data"]["revocRegDefId"]
+        timestamp = tx_raw["data"]["txnMetadata"]["txnTime"]
+        delta_req = indy_vdr.ledger.build_get_revoc_reg_delta_request(submitter_did=None, revoc_reg_id=rev_reg_id, from_ts=None, to_ts=timestamp)
+        res = await self.pool.submit_request(delta_req)
+        return res
+        
+
+def main():
+    tester = Tester(genesis_url="https://raw.githubusercontent.com/idunion/idunion_testnet_genesis/master/pool_transactions_genesis")
+    
+
+    print(asyncio.get_event_loop().run_until_complete(tester.get_from_tx(843)))
+
+if __name__ == "__main__":
+    main()
