349b95a38d1773f81b48b562a4196abd220a2870
Christian Bormann <chris.bormann@gmx.de>
2022-09-07 11:41:34 +0200

    Some more utility for manual lookups of transactions

--- python-tests/test.py
+++ python-tests/test.py
@@ -22,20 +22,24 @@ class Tester:
         except Exception as err:
             print(f"Could not initialize pool: {err=}")
     
+
+    async def get_delta(self, id: str, ts: int) -> dict:
+        request = indy_vdr.ledger.build_get_revoc_reg_delta_request(submitter_did=None, revoc_reg_id=id, from_ts=None, to_ts=ts)
+        result = await self.pool.submit_request(request)
+        return result
+
     async def get_from_tx(self, tx_id: int) -> dict:
         tx_req = indy_vdr.ledger.build_get_txn_request(None, 1, tx_id)
         tx_raw = await self.pool.submit_request(tx_req)
         rev_reg_id = tx_raw["data"]["txn"]["data"]["revocRegDefId"]
         timestamp = tx_raw["data"]["txnMetadata"]["txnTime"]
-        delta_req = indy_vdr.ledger.build_get_revoc_reg_delta_request(submitter_did=None, revoc_reg_id=rev_reg_id, from_ts=None, to_ts=timestamp)
-        res = await self.pool.submit_request(delta_req)
+        res = await self.get_delta(id=rev_reg_id, ts=timestamp)
         return res
         
 
 def main():
     tester = Tester(genesis_url="https://raw.githubusercontent.com/idunion/idunion_testnet_genesis/master/pool_transactions_genesis")
     
-
     print(asyncio.get_event_loop().run_until_complete(tester.get_from_tx(843)))
 
 if __name__ == "__main__":
